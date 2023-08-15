5f42b79d6b0188a701c37d97b391a288f4ead108
Divyank-Gupta-g <121607164+Divyank-Gupta-g@users.noreply.github.com>
2023-08-15 10:44:04 +0530

    This program to exercise the call of static and non-static methods. A partial code is given defining two methods, namely sum( ) and multiply ( ). You have to call these methods to find the sum and product of two numbers. Complete the code segment as instructed.
    Week 3 Assignment 4

---
+++ Exercise3_4.java
@@ -0,0 +1,26 @@
+//This program to exercise the call of static and non-static methods. A partial code is given defining two methods, namely sum( ) and multiply ( ). You have to call these methods to find the sum and product of two numbers. Complete the code segment as instructed.
+
+import java.util.Scanner;
+
+class QuestionScope {
+	int sum(int a, int b){ //non-static method
+        	return a + b;
+    	}
+	static int multiply(int a, int b){ //static method
+        	return a * b;
+    	}
+}
+
+public class Exercise3_4{
+	public static void main( String[] args ) {
+        	Scanner sc = new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		
+		//Called the method sum() to find the sum of two numbers.
+		//Called the method multiply() to find the product of two numbers.
+		QuestionScope q = new QuestionScope();
+		System.out.println(q.sum(n1, n2));
+		System.out.print(QuestionScope.multiply(n1, n2));
+	}
+}
