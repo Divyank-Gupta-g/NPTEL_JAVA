d02b4a54b4e84686186ab8ede4bfcfb5e2766613
Divyank-Gupta-g <121607164+Divyank-Gupta-g@users.noreply.github.com>
2023-08-16 14:40:50 +0530

    print current year.
    Complete the code segment to print the current year. Your code should compile successfully.

---
+++ JAVA_4_2.java
@@ -0,0 +1,17 @@
+//Complete the code segment to print the current year. Your code should compile successfully.
+
+// The following is the declaration of the main class named Question42
+public class JAVA_4_2{ 
+	public static void main(String args[]){
+		int year; 	// integer type variable to store year	
+              
+				// Create an object of Calendar class. 
+		java.util.Calendar current;
+				// Use getInstance() method to initialize the Calendar object.
+		current=java.util.Calendar.getInstance();
+				// Initialize the int variable year with the current year 
+		year= current.get(java.util.Calendar.YEAR);
+				// Print the current Year
+		System.out.println("Current Year: "+year);
+	}  
+}
